f13aaa4baeb8e1ca8df2cd777610c5abb3a74c98
Toshitsugu Takahashi <cist.takahashi@gmail.com>
2016-04-25 10:11:22 +0900

    session処理を修正.

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
+	session_start();
+    if (!empty($_SESSION) || isset($_SESSION['accountname'])) {
+    	header('Location: ./bbs.php');
+    }
+
 	if (!empty($_POST) && isset($_POST['logout'])) {
-		session_start();
 		// セッション変数を全て解除する
 		$_SESSION = array();
 		// セッションを切断するにはセッションクッキーも削除する。
 		// Note: セッション情報だけでなくセッションを破壊する。
 		if (ini_get("session.use_cookies")) {
 		    $params = session_get_cookie_params();
-		    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+		    setcookie(session_name(), '', time() - 42000, '/');
 
 			// 最終的に、セッションを破壊する
 			session_destroy();
 		}
 	}
-    if (!empty($_SESSION) || isset($_SESSION['accountname'])) {
-      header('Location: ./bbs.php');
-    }
 	
 	require('./dbconnect.php');
 	$rec = false;
